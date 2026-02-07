ca74df03e0d0cd8e5f57e512d56162650ed927ee
enamul <enamul.dev01@gmail.com>
2026-02-07 23:53:29 +0600

    danteox

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='w-full h-screen flex items-center justify-center '>
+      <span className='text-5xl font-bold'>Muhammad Enamul Hossain</span>
+        <span className="text-rotate text-5xl duration-4000">
+          <span className="justify-items-center">
+              <span className="font-bold  px-2">Emon</span>
+              <span className="font-bold  px-2">Rumi</span>
+              <span className="font-bold  px-2">Dante</span>
+          </span>
+        </span>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
