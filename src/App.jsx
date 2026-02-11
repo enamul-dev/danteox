6e8452f9124f07e6ae2ac7f100b11249b2e33935
enamul <enamul.dev01@gmail.com>
2026-02-11 10:26:10 +0600

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import '/App.css'
+
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,8 +8,8 @@ function App() {
   return (
     <>
     <div className='w-full h-screen flex items-center justify-center '>
-      <span className='text-5xl font-bold'>Muhammad Enamul Hossain</span>
-        <span className="text-rotate text-5xl duration-4000">
+      <span className='text-2xl md:text-5xl font-bold'>Muhammad Enamul Hossain</span>
+        <span className="text-rotate text-2xl md:text-5xl duration-4000">
           <span className="justify-items-center">
               <span className="font-bold  px-2">Emon</span>
               <span className="font-bold  px-2">Rumi</span>
